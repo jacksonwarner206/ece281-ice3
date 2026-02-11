ac20dcdb95d1a14a5292d13a25d6b3093ff1b24c
jacksonwarner206 <c28jackson.warner@afacademy.af.edu>
2026-02-11 15:32:32 -0700

    changed a test

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -57,6 +57,8 @@ begin
 	       assert (w_sum = x"A" and w_Cout = '0') report "bad with 4+5+1" severity failure;
 	   w_addends <= x"45"; w_Cin <= '0'; wait for 10 ns;
 	       assert (w_sum = x"9" and w_Cout = '0') report "bad with 4+5+0" severity failure;
+	   w_addends <= x"ff"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"E" and w_Cout = '1') report "bad with f+f+0" severity failure;
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
