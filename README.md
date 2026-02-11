bfa42970e37bfaa2c727c3450233cb628152b629
jacksonwarner206 <c28jackson.warner@afacademy.af.edu>
2026-02-11 15:19:32 -0700

    changed the readme file

--- README.md
+++ README.md
@@ -22,3 +22,9 @@ Then it **elaborates** the entity defined by `$TB_ENTITY`
 Finally, the workflow **runs** the simulation. If successful then it will quietly exit with a `0` code.
 If any of the `assert` statements fail then GHDL will cease the simulation and exit with non-zero code; this will also cause the workflow to fail.
 Assert statements of other severity levels will be reported, but not fail the workflow.
+
+![cases 15+15+1, 0+0+0, 9+4+1, 0+10+0, 4+5+1, and 4+5+0 tested](/c/Users/C28Jackson.Warner/OneDrive - afacademy.af.edu/Pictures/Screenshots/Screenshot 2026-02-11 150210.png)
+
+## Documentation 
+
+I looked up how to get a .png into a .md file on google.
--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -49,7 +49,14 @@ begin
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
 	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
-	
+	   w_addends <= x"94"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"E" and w_Cout = '0') report "bad with 9+4+1" severity failure;
+       w_addends <= x"0A"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"A" and w_Cout = '0') report "bad with 0+10+0" severity failure;
+	   w_addends <= x"45"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"A" and w_Cout = '0') report "bad with 4+5+1" severity failure;
+	   w_addends <= x"45"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"9" and w_Cout = '0') report "bad with 4+5+0" severity failure;
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
